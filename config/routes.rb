a69ad926585007b05e65d22454a2208d750129bc
adyrda2 <adyrda2@gmail.com>
2016-05-04 08:49:12 -0500

    add home page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :entries do 
     resources :comments
   end
+  root 'entries#index'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
