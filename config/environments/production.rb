a99d277662b2509278748d862c81811af0db562e
adyrda2 <adyrda2@gmail.com>
2016-05-30 00:04:26 -0500

    change domain to gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
   address: "smtp.gmail.com",
   port: 587,
-  domain: "https://dry-wildwood-68370.herokuapp.com",
+  domain: "gmail.com",
   authentication: "plain",
   enable_starttls_auto: true,
   user_name: ENV["GMAIL_USERNAME"],
