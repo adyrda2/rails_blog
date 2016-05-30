66baeef9adfd11e0c2c07ca79d297e3d48e971a3
adyrda2 <adyrda2@gmail.com>
2016-05-29 23:16:36 -0500

    set up confirmable in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,12 +75,14 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # Do not dump schema after migrations.
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true 
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.smtp_settings = {
   address: "smtp.gmail.com",
   port: 587,
-  domain: "https://immense-headland-63342.herokuapp.com/
-",
+  domain: "gmail.com",
   authentication: "plain",
   enable_starttls_auto: true,
   user_name: ENV["GMAIL_USERNAME"],
