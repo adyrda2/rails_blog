c3a4479a7388878d5d5044189b8a4e9dfd4db973
adyrda2 <adyrda2@gmail.com>
2016-05-29 23:22:56 -0500

    Add heroku domain to environment

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,6 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
+config.action_mailer.default_url_options = { :host => 'https://dry-wildwood-68370.herokuapp.com/' }
 
 # Initialize the Rails application.
 Rails.application.initialize!
