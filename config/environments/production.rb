6cbaabfc10b5f38d572bc1c2363c44fdd546f90d
adyrda2 <adyrda2@gmail.com>
2016-05-30 00:36:05 -0500

    add default url option

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,7 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # Do not dump schema after migrations.
+  Rails.application.routes.default_url_options[:host] = 'https://dry-wildwood-68370.herokuapp.com'
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.smtp_settings = {
   address: "smtp.gmail.com",
